829f311e780bc1fce233cc97450716c886d3f153
jcepriano <jamescepriano@gmail.com>
2023-09-06 12:29:42 -0700

    made model change and secured connection string

--- MvcMessageLogger/Controllers/UsersController.cs
+++ MvcMessageLogger/Controllers/UsersController.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using MvcMessageLogger.DataAccess;
 using MvcMessageLogger.Models;
-using System.Runtime.Intrinsics.X86;
 
 namespace MvcMessageLogger.Controllers
 {
